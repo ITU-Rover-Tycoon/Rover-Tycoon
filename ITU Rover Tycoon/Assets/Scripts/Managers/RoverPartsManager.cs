92a4b8d27f21f99c501f62c650efcd2b2b88c31b
YAVUZ <terzioglu16@itu.edu.tr>
2021-05-02 01:37:23 +0300

    branch içi commit deneme 1

--- ITU Rover Tycoon/Assets/Scripts/Managers/RoverPartsManager.cs
+++ ITU Rover Tycoon/Assets/Scripts/Managers/RoverPartsManager.cs
@@ -7,7 +7,7 @@ namespace Managers
         // Start is called before the first frame update
         void Start()
         {
-        
+        //hello world
         }
 
         // Update is called once per frame
