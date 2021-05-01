9f272297b327a5fb96da1472b258fde382c752e0
Kyun <gokcankrc@gmail.com>
2021-05-01 21:49:28 +0300

    creating skeleton is half done

---
+++ ITU Rover Tycoon ®™©/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Library;
+
+
+
+namespace Managers
+{
+    
+    public class GameManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            int c;
+            c = (int) MembersManager.MyEnum.abc;
+            
+            Debug.Log("printing c");
+            Debug.Log(c.ToString());
+            InventoryLibrary.Item.printItem();
+            InventoryLibrary.Item a;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
